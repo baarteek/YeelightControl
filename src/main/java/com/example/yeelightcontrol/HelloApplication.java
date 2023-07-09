3fffab76bf8c75d436a78bb822e624c1551a4179
Bartek Krukowski <bartekkrukowski822@gmail.com>
2023-07-09 13:46:10 +0200

    added connect ui

--- src/main/java/com/example/yeelightcontrol/HelloApplication.java
+++ src/main/java/com/example/yeelightcontrol/HelloApplication.java
@@ -3,6 +3,7 @@ package com.example.yeelightcontrol;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +12,12 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 500, 700);
+        scene.getStylesheets().add(getClass().getResource("/com/example/yeelightcontrol/css/style.css").toExternalForm());
+        stage.setResizable(false);
+        Image applicationIcon = new Image(getClass().getResourceAsStream("/com/example/yeelightcontrol/icons/bulb.png"));
+        stage.getIcons().add(applicationIcon);
+        stage.setTitle("Yeelight Control");
         stage.setScene(scene);
         stage.show();
     }
